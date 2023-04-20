400ef0f5eb2a219376407f5804caef5f7a44ccc0
Trased <trased0998@gmail.com>
2023-04-20 23:11:08 +0300

    Minor update
    Optimised collision Checker.
    Added an image that will be set as background in future

--- PAOOGAME/src/PaooGame/Items/collisionChecker.java
+++ PAOOGAME/src/PaooGame/Items/collisionChecker.java
@@ -7,12 +7,10 @@ import static java.lang.Math.sqrt;
 
 public class collisionChecker {
     RefLinks ref;
-    Map map;
     public collisionChecker(RefLinks reflink){
         ref = reflink;
-        map = new Map(reflink);
     }
-    public void checkTile(Hero entity) {
+    public void checkTile(Character entity) {
         int entityLeft = (int) (entity.GetX() + entity.normalBounds.x);
         int entityRight = (int) (entity.GetX() + entity.normalBounds.x + entity.normalBounds.width);
         int entityUp = (int) (entity.GetY() + entity.normalBounds.y);
@@ -25,22 +23,22 @@ public class collisionChecker {
 
 
         if (ref.GetKeyManager().up) {
-            if (map.GetTile(entityLeftCol, entityUpCol).IsSolid() && map.GetTile(entityRightCol, entityUpCol).IsSolid()) {
+            if (ref.GetMap().GetTile(entityLeftCol, entityUpCol).IsSolid() && ref.GetMap().GetTile(entityRightCol, entityUpCol).IsSolid()) {
                 entity.collisionOn = true;
             }
         }
         if (ref.GetKeyManager().down) {
-            if (map.GetTile(entityLeftCol, entityDownCol).IsSolid() && map.GetTile(entityRightCol, entityDownCol).IsSolid()) {
+            if (ref.GetMap().GetTile(entityLeftCol, entityDownCol).IsSolid() && ref.GetMap().GetTile(entityRightCol, entityDownCol).IsSolid()) {
                 entity.collisionOn = true;
             }
         }
         if (ref.GetKeyManager().right) {
-            if (map.GetTile(entityRightCol, entityUpCol).IsSolid() && map.GetTile(entityRightCol, entityDownCol).IsSolid()) {
+            if (ref.GetMap().GetTile(entityRightCol, entityUpCol).IsSolid() && ref.GetMap().GetTile(entityRightCol, entityDownCol).IsSolid()) {
                 entity.collisionOn = true;
             }
         }
         if (ref.GetKeyManager().left) {
-            if (map.GetTile(entityLeftCol, entityUpCol).IsSolid() && map.GetTile(entityLeftCol, entityDownCol).IsSolid()) {
+            if (ref.GetMap().GetTile(entityLeftCol, entityUpCol).IsSolid() && ref.GetMap().GetTile(entityLeftCol, entityDownCol).IsSolid()) {
                 entity.collisionOn = true;
             }
         }
